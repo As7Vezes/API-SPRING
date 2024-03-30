3baa3280e0ac9628f6e7ebc13ec99646f7d15136
As7Vezes <denysn60@gmail.com>
2024-03-30 16:59:50 -0300

    feat: initialized project setup.

---
+++ src/main/java/com/client/ws2000plus/Ws2000PlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws2000plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws2000PlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws2000PlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/ws2000plus/Ws2000PlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.ws2000plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws2000PlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
